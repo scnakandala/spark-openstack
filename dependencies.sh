8c8240ffabd7f635930645de9c98a5f12964cbac
Supun Nakandala <supun.nakandala@gmail.com>
2019-03-07 12:23:25 -0800

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -5,4 +5,4 @@ sudo apt-get -y install libffi-dev libssl-dev python-dev
 sudo apt-get -y install python-pip
 sudo pip install --upgrade pip
 sudo pip install os_client_config
-sudo pip install -r pip-lists.txt
+sudo pip install -r pip-list.txt
