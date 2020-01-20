e669931392e0055c5cb8e15d545b6b76b1d18cd3
Supun Nakandala <supun.nakandala@gmail.com>
2020-01-20 08:57:00 +0000

    updating to work with Spark 2.4

--- ansible/spark_openstack_cmd.py
+++ ansible/spark_openstack_cmd.py
@@ -13,6 +13,7 @@ import urlparse
 
 spark_versions = \
     {
+	"2.4.4": {"hadoop_versions": ["2.7","3.0", "3.1"]},
         "2.2.0": {"hadoop_versions": ["2.6", "2.7"]},
         "2.1.0": {"hadoop_versions": ["2.3", "2.4", "2.6", "2.7"]},
         "2.0.2": {"hadoop_versions": ["2.3", "2.4", "2.6", "2.7"]},
